adff9f0c2903247ce5dd000195e5e1fe03e2f86f
atomiciscool <146490689+atomiciscool@users.noreply.github.com>
2024-02-16 21:29:22 +0100

    Update main.e7a33c55.chunk.js

--- static/js/main.e7a33c55.chunk.js
+++ static/js/main.e7a33c55.chunk.js
@@ -12139,7 +12139,7 @@
               },
               metrics: {
                 shadowMapSize: 4,
-                accel: 120,
+                accel: 1000,
                 reverse: 5,
                 jerk: 48,
                 brake: 12,
@@ -12180,7 +12180,7 @@
               metrics: {
                 shadowMapSize: 4,
                 steerSpeed: 1.57,
-                accel: 120,
+                accel: 10000,
                 reverse: 5,
                 jerk: 48,
                 brake: 12,
@@ -12246,7 +12246,7 @@
               metrics: {
                 shadowMapSize: 8,
                 steerSpeed: 0.45,
-                accel: 120,
+                accel: 1000,
                 reverse: 5,
                 jerk: 6,
                 brake: 12,
@@ -12313,7 +12313,7 @@
               metrics: {
                 shadowMapSize: 4,
                 steerSpeed: 1.57,
-                accel: 120,
+                accel: 1000,
                 reverse: 5,
                 jerk: 48,
                 brake: 12,
@@ -12354,7 +12354,7 @@
               metrics: {
                 shadowMapSize: 4,
                 steerSpeed: 1.57,
-                accel: 120,
+                accel: 1000,
                 reverse: 5,
                 jerk: 48,
                 brake: 12,
